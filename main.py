1fffd878eb9db243ef6ea47c3b894bd9eae66a1a
Ayu <y.abouseada16@gmail.com>
2021-04-07 17:18:23 +0200

    V 0.2

--- main.py
+++ main.py
@@ -12,4 +12,27 @@ user_iq = 190
 print(user_iq)
 user_age = (user_iq/4)
 print(user_age)
+#augmented assignmet operator
+some_value = 5
+some_value +=2
+some_value *=2
+some_value -=2
+print(some_value)
+#Strings
+long_string = '''
+WOW
+ OO
+ ---
+'''
+print(long_string)
+#String Concatenation
+print('Hello'+' Jhon')
+#Conversion
+print(type(str(100)))
+a= str(100)
+b= int(a)
+c= type(b)
+d= type (a)
+print(c)
+print (d)
 
